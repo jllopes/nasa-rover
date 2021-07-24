bae29684ea4203b73e6641f78f80601e10ee5405
João Lopes <jlglopes96@gmail.com>
2021-07-24 22:14:34 +0100

    Add test for multiple instructions input

--- src/test/scala/io/github/jllopes/nasarover/NavigationSpec.scala
+++ src/test/scala/io/github/jllopes/nasarover/NavigationSpec.scala
@@ -14,10 +14,20 @@ class NavigationSpec extends CatsEffectSuite {
   test("Navigate endpoint returns 0,1,N after moving forward once") {
     val body = """{"command": "F"}"""
     val call = Request[IO](Method.POST, uri"/navigate").withEntity(body)
-    val positionRef = Ref[IO].of(Position(Coordinates(0,0), North))
+    val positionRef = Ref[IO].of(Position(Coordinates(0,0), North)) // TODO: Refactor to avoid repetition
     val navigationServices = NavigationServices.impl[IO](positionRef)
     val navigation = Navigation.impl[IO](navigationServices)
     val test = NasaRoverRoutes.navigationRoutes(navigation).orNotFound(call)
     assertIO(test.flatMap(_.as[String]), """"0,1,N"""")
+  }
+
+  test("Navigate endpoint returns 1,0,E after rotating right and moving forward once") {
+    val body = """{"command": "RF"}"""
+    val call = Request[IO](Method.POST, uri"/navigate").withEntity(body)
+    val positionRef = Ref[IO].of(Position(Coordinates(0,0), North))
+    val navigationServices = NavigationServices.impl[IO](positionRef)
+    val navigation = Navigation.impl[IO](navigationServices)
+    val test = NasaRoverRoutes.navigationRoutes(navigation).orNotFound(call)
+    assertIO(test.flatMap(_.as[String]), """"1,0,E"""")
   }
 }
