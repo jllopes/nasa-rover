e35f3d8192dd7f2cf383e42b87b5358b247b2cdb
João Lopes <jlglopes96@gmail.com>
2021-07-24 22:32:53 +0100

    Add initial movement and rotation implementation

--- src/main/scala/io/github/jllopes/nasarover/services/NavigationServices.scala
+++ src/main/scala/io/github/jllopes/nasarover/services/NavigationServices.scala
@@ -14,7 +14,45 @@ object NavigationServices {
 
   def impl[F[_] : Effect](positionRef: F[Ref[F, Position]]): NavigationServices[F] = new NavigationServices[F] {
     def navigate(commandList: List[Char]): F[Position] = {
-      Position(Coordinates(0,1), North).pure[F]
+      for {
+        position <- positionRef
+        _ <- commandList.traverse {
+          case 'F' => move(isForwardMove = true)(position)
+          case 'B' => move(isForwardMove = false)(position)
+          case 'R' => rotate(isRightTurn = true)(position)
+          case 'L' => rotate(isRightTurn = false)(position)
+        }
+        result <- position.get
+      } yield {
+        result
+      }
+    }
+  }
+
+  private def move[F[_] : Effect](isForwardMove: Boolean)(positionRef: Ref[F, Position]): F[Unit] = {
+    positionRef.update {
+      currentPosition =>
+        val (xMovement, yMovement) = currentPosition.direction match {
+          case North => if (isForwardMove) (0, 1) else (0, -1)
+          case South => if (isForwardMove) (0, -1) else (0, 1)
+          case East => if (isForwardMove) (1, 0) else (-1, 0)
+          case West => if (isForwardMove) (-1, 0) else (1, 0)
+        }
+        val updatedCoordinates = currentPosition.coordinates.copy(currentPosition.coordinates.x + xMovement, currentPosition.coordinates.y + yMovement)
+        currentPosition.copy(coordinates = updatedCoordinates)
+    }
+  }
+
+  private def rotate[F[_] : Effect](isRightTurn: Boolean)(positionRef: Ref[F, Position]): F[Unit] = {
+    positionRef.update {
+      currentPosition =>
+        val newDirection = currentPosition.direction match {
+          case North => if (isRightTurn) East else West
+          case South => if (isRightTurn) West else East
+          case East => if (isRightTurn) South else North
+          case West => if (isRightTurn) North else South
+        }
+        currentPosition.copy(direction = newDirection)
     }
   }
 }
